e113d1c0dc2fe29c242207a629dc003dad44a98a
Сергей Карасев <karasev-s@internet.ru>
2024-09-21 16:51:42 +0300

    Add files via upload

---
+++ Introspekcia.py
@@ -0,0 +1,31 @@
+
+def introspection_info(obj):
+    # Определение типа объекта
+    obj_type = type(obj).__name__
+
+    # Получение атрибутов объекта
+    attributes = dir(obj)
+
+    # Получение методов объекта
+    methods = [method for method in attributes if callable(getattr(obj, method))]
+
+    # Определение модуля, к которому объект принадлежит
+    module = obj.__class__.__module__
+
+    # Создание словаря с информацией об объекте
+    info = {'type': obj_type, 'attributes': attributes, 'methods': methods, 'module': module}
+
+
+    return info
+
+# Интроспекция числа
+number_info = introspection_info(42)
+print(number_info)
+
+# Интроспекция строки
+string_info = introspection_info('Sergey')
+print(string_info)
+
+# Интроспекция списка
+list_info = introspection_info([1, 20, 4.0, 'Karasev'])
+print(list_info)
